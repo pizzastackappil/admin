9fde6939bfcf59c1c46aae4e613e79b3eb635b45
davidAffman <affmandavid@gmail.com>
2024-02-11 00:04:55 +0200

    add menu create

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Admin, DataProvider, EditGuesser, Loading, Resource } from "react-admin";
 import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
 import { MenuEdit } from "./modules/menu/components/menu-edit/menu-edit.component";
+import { MenuCreate } from "./modules/menu/components/menu-create/menu-create.component";
 
 function App() {
   const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(
@@ -28,7 +29,7 @@ function App() {
       <CssBaseline />
 
       <Admin dataProvider={dataProvider}>
-        <Resource name="menu" list={MenuList} edit={MenuEdit} />
+        <Resource name="menu" list={MenuList} edit={MenuEdit} create={MenuCreate} />
       </Admin>
     </>
   );
